09fb0c2920c63492502153319dd8cf4f42fbe403
Malcom X <Taajlogan@gmail.com>
2024-04-10 16:12:36 -0400

    addedSaveBtnFctn

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Display today's date
 var todayDate = dayjs().format('dddd, MM D YYYY');
 $("#currentday").html(todayDate);
+
+$(document).ready(function () {
+// Save Button Evnt listener Click
+    $(".saveBtn").on("click, function() {
+    // get the text input and time value 
+     var text = $(this).siblings(".description").val();
+     var time = $(this).parent().attr("id");
+     
+     //Save to local storage 
+     localStorage.setItem(time, text);
+
+});
